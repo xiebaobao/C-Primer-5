22e4c872bd12f2960cc8af8ec89f05aaa270784b
xtx <13151562558@163.com>
2017-11-20 17:43:55 +0800

    使用标准输入和输出计算输入数值结果和

---
+++ chapter1/iostream_test.cpp
@@ -0,0 +1,21 @@
+/*************************************************************************
+	> File Name: iostream_test.cpp
+	> Author: xtx
+	> Mail: 13151562558@163.com 
+	> Created Time: Mon 20 Nov 2017 05:39:45 PM CST
+ ************************************************************************/
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    cout<<"input two number"<<endl;
+    int a = 0, b = 0;
+    cin>>a>>b;
+    cout<<"a="<<a<<"  b="<<b
+        <<" a+b="<<a+b<<endl;
+    return 0;
+}
