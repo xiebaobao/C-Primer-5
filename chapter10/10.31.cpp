434119c3e0371b45e3e250be4b2c8ca0da3d3673
xtx <13151562558@163.com>
2017-11-29 11:23:22 +0800

    重写上一题的程序，打印不重复的元素

---
+++ chapter10/10.31.cpp
@@ -0,0 +1,35 @@
+/*************************************************************************
+	> File Name: 10.30.cpp
+	> Author: xtx
+	> Mail: 13151562558@163.com 
+	> Created Time: Wed 29 Nov 2017 11:00:11 AM CST
+ ************************************************************************/
+
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include <algorithm>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+using std::copy;
+using std::istream_iterator;
+using std::ostream_iterator;
+using std::sort;
+using std::unique_copy;
+
+int main()
+{
+    vector<int> vec;
+    istream_iterator<int> in1(cin);
+    istream_iterator<int> end; // eof
+    copy(in1, end, back_inserter(vec));
+
+    ostream_iterator<int> out1(cout, " ");
+    sort(vec.begin(), vec.end());
+    unique_copy(vec.begin(), vec.end(), out1);
+   // copy(vec.begin(), vec.end(), out1);
+    return 0;
+
+}
